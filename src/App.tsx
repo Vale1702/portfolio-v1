266bc83f6ce4cd24098f258c486032584a19b967
Valentina <valentinahdz0296@gmail.com>
2026-02-10 20:50:22 -0600

    creacion de estructura en Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
+import Header from './components/Header'
+import Home from './pages/Home'
+
+
 function App() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center gap-4 bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">
-        Azul
-      </h1>
-
-      <h2 className="text-2xl font-semibold text-red-500">
-        Rojo
-      </h2>
-
-      <p className="text-lg text-green-600">
-        Verde
-      </p>
-
-      <div className="bg-purple-600 text-white px-6 py-3 rounded">
-        Morado
-        hola
-        <p className="text-center font-bold"></p>
-      </div>
-    </div>
+    <>
+    <Header/>
+    <Home/>
+    </>
   )
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+        <div className="flex flex-col p-4">
+        <p className="text-center font-bold text-3xl">Header</p>
+        </div>
+    )
+}
+
+export default Header;
