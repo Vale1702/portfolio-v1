58fecbdc5181d67c6329a9e2bc06642d5adfee25
Valentina <valentinahdz0296@gmail.com>
2026-03-23 17:12:58 -0600

    Modificacion en estilos para seccion de testimonio

--- src/components/TestimonialCard.tsx
+++ src/components/TestimonialCard.tsx
@@ -4,13 +4,14 @@ type TestimonialCardProps = Omit<Testimonial, "id">
 
 function TestimonialCard ({imageWebp, icon, text, name, role}:TestimonialCardProps){
     return(
-        <div className="testimonial-card">
+        <div className="testimonial-card group">
             <div className="testimonial-card__header">
                 <picture className="testimonial-card__logo">
                     <source 
                     type ="image/webp"
                     />
-                    <img src={imageWebp} alt={name} />
+                    <img src={imageWebp} alt={name} className="testimonial-card__avatar"
+                    loading="lazy"/>
                 </picture>
                 <img src={icon} alt="Quote" className="testimonial-card__quote-icon" 
                 /> 
